79402fb5d3e9624dd1f26489c973a366a5bb292a
SwinJoy <qq1066684692>
2017-09-19 09:14:18 +0800

    打了很多

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/SpalshActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/SpalshActivity.java
@@ -2,9 +2,10 @@ package cn.edu.gdmec.android.mobileguard;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SpalshActivity extends AppCompatActivity {
-
+ private TextView mTvVersion;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
